831cf8705f27b67e280080bccdd0ded4dfd2e288
BuildTools <dani.matev123@gmail.com>
2021-07-08 16:42:24 +0300

    added eventsAPI connection

--- bot.py
+++ bot.py
@@ -2,11 +2,23 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from slackeventsapi import SlackEventAdapter
+from flask import Flask
 
 #better to load it from the .env file because it's sensitive info
 env_path = Path(',') / '.env'
 load_dotenv()
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN']);
+app = Flask(__name__)
+slack_events_adapter = SlackEventAdapter(
+    os.environ['SIGNING_SECRET'],'/slack/events', app)
 
-client.chat_postMessage(channel='#test', text="Hello world")
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+
+#response = client.chat_postMessage(channel='#test', text="Hello world!")
+
+# assert response["ok"]
+# assert response["message"]["text"] == "Hello world!"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
